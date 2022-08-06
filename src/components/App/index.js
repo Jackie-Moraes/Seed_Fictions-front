c358b7c995b506cd28fdb0627e440405f58b8d9c
Jackie-Moraes <patrickandradepatrick@hotmail.com>
2022-08-06 13:26:53 -0300

    setup

---
+++ src/components/App/index.js
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+import "../../assets/styles/reset.css"
+import GlobalStyle from "../../globalStyles"
+
+import DataContext from "../context/context.js"
+
+import Home from "../Home"
+
+export default function App() {
+    return (
+        <DataContext.Provider value={{ data, setData }}>
+            <GlobalStyle />
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                </Routes>
+            </BrowserRouter>
+        </DataContext.Provider>
+    )
+}
---
+++ src/components/Home/index.js
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+import Header from "../Header"
+
+export default function Home() {
+    return (
+        <>
+            <Header />
+            <main></main>
+        </>
+    )
+}
